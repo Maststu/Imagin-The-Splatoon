c49eba47fcfaa0d6eb2e3845f9ace71ced83afe3
Maststu <108624944+Maststu@users.noreply.github.com>
2022-12-16 15:28:47 +0900

    Add files via upload

--- webviewer/app.py
+++ webviewer/app.py
@@ -1,5 +1,6 @@
 import json
 from pprint import pprint
+
 from flask import (Flask, current_app, g, redirect, render_template, request,
                    url_for)
 
@@ -13,15 +14,25 @@ def Home():
 def login():
     return render_template("login.html")
 
+@app.route("/show_winrate")
+def show_winrate():
+    with open('./templates/results.json') as f:
+        df = json.load(f)
+    
+    return render_template("show_winrate.html",df=df)
 
+@app.route('/name_search', methods=["POST"])
+def name_search():
+    with open('./templates/results.json') as f:
+        df = json.load(f)
+    
+    return render_template("name_search.html",df=df)
 
 @app.route("/show_json")
 def show_json():
     with open('./templates/results.json') as f:
         df = json.load(f)
-    file =open('./templates/test.json','w')
-    
-    sum_kill,sum_death,sum_assist=0,0,0
+    file=open('./templates/test.json','w')
     df_team = df[124]['data']['vsHistoryDetail']
     
     for i in range(len(df_team['myTeam']['players'])):
@@ -30,9 +41,8 @@ def show_json():
         df_team['myTeam']['players'][i].pop('headGear')
         df_team['myTeam']['players'][i].pop('shoesGear')
         df_team['myTeam']['players'][i].pop('nameplate')
-    
-    pprint(df_team,stream=file)
+    file.write(json.dumps(df_team,sort_keys=True,indent=4))    
     file.close()
-    return render_template("show_json.html",df=df,sum_kill=sum_kill,sum_death=sum_death,sum_assist=sum_assist)
+    return render_template("show_json.html",df=df)
 
 
