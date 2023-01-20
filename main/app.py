0b7d8334b294719e3a549d1963f3fe948a43a197
Maststu <108624944+Maststu@users.noreply.github.com>
2023-01-20 13:04:20 +0900

    Add files via upload

---
+++ main/app.py
@@ -0,0 +1,102 @@
+import json
+from pprint import pprint
+import pymongo
+import my_s3s
+from flask import (Flask, current_app, g, redirect, render_template, request,
+                    url_for) 
+
+app = Flask(__name__)
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+Spladb = myclient["Spla3db"]
+game_col = Spladb["Game"]
+@app.route("/")
+def Home():
+    #database
+    
+    return render_template("home.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/show_winrate",methods=["GET","POST"])
+def show_winrate():
+    with open('./templates/results.json') as f:
+        df = json.load(f)
+    
+    if request.method == 'POST':
+        weapon=request.form['weapon']
+    else:
+        weapon =request.args.get('weapon','all')
+        
+        
+    return render_template("show_winrate.html",df=df,weapon=weapon)
+
+@app.route('/name_search',methods=["GET","POST"])
+def name_search():
+    with open('./templates/results.json') as f:
+        df = json.load(f)
+    
+    if request.method == 'POST':
+        username = request.form['username']
+    else:
+        username = request.args.get('username', 'noname')
+    return render_template("name_search.html", df=df, username=username)
+
+
+
+
+@app.route('/config',methods=["POST"])
+def config():
+    return request.form.get('user')
+    
+
+@app.route("/show_json",methods=["GET","POST"])
+def show_json():
+    # with open('./templates/results.json') as f:
+    #     df = json.load(f)
+    # file=open('./templates/test.json','w')
+    # df_team = df[124]['data']['vsHistoryDetail']
+    
+    # for i in range(len(df_team['myTeam']['players'])):
+        
+    #     df_team['myTeam']['players'][i].pop('clothingGear')
+    #     df_team['myTeam']['players'][i].pop('headGear')
+    #     df_team['myTeam']['players'][i].pop('shoesGear')
+    #     df_team['myTeam']['players'][i].pop('nameplate')
+    # file.write(json.dumps(df_team,sort_keys=True,indent=4))    
+    # file.close()
+
+    if request.method == 'POST':
+
+        redirect('login')
+        my_s3s.set_language()
+
+        my_s3s.fetch_json("ink",separate=True, exportall=True, specific=True, skipprefetch=True)
+
+    df = game_col.find().sort("playedTime",-1)
+    return render_template("show_json.html",df=df)
+
+@app.route("/show_all_json")
+def show_all_json():
+    with open('./templates/all.json') as f:
+        df = json.load(f)
+
+    df_player = df['data']['vsHistoryDetail']['myTeam']['players']
+    
+    return render_template("show_all_json.html",df=df_player)
+    return "hello"
+
+
+
+@app.route('/compatibility',methods=["GET","POST"])
+def compatibility():
+    with open('./templates/results.json') as f:
+        df = json.load(f)
+
+    if request.method == 'POST':
+        username = request.form['username']
+    else:
+        username = request.args.get('username', 'noname')
+    return render_template("compatibility.html", df=df, username=username)
